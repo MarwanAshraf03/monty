e7c77149584587f565a9cf46b9ade3192307e72b
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-08-25 02:31:50 +0300

    update

--- bytecodes/19.m
+++ bytecodes/19.m
@@ -1,6 +1,5 @@
 push 1
-push 2
-push 10
-push 0
+push 6
+push 3
 div
 pall
--- op_codes2.c
+++ op_codes2.c
@@ -84,7 +84,7 @@ void _div(stack_t **stack, unsigned int line_number)
 
 	if (!*stack || !stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	temp1 = (*stack);
